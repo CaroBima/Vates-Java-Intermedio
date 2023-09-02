511c8add6de6bd95ebfa41d170b1aefb42f58ddf
Caro Bima <carobima@gmail.com>
2023-09-02 05:14:48 -0300

    Agregada la resolución del ejercicio de multiplicacion (Clase 2)

--- src/main/java/com/curso/clase2/multiplicacion/Multiplicar.java
+++ src/main/java/com/curso/clase2/multiplicacion/Multiplicar.java
@@ -1,10 +1,29 @@
 package com.curso.clase2.multiplicacion;
 
+import java.util.Scanner;
+
 /**- Escribe un programa que genere una tabla de multiplicar del 1 al 10 para un número ingresado por el usuario.
  *
  */
 public class Multiplicar {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero = 0;
 
+        System.out.print("Ingrese un número entero: ");
+
+        while (true) {
+            if (scanner.hasNextInt()) {
+                numero = scanner.nextInt();
+                break; // Sale del bucle si ingresa un int
+            } else {
+                System.out.print("Entrada no válida. Ingresa un número entero: ");
+                scanner.next(); // Limpia el búfer de entrada
+            }
+        }
+
+        for(int i = 1; i<=10; i++ ){
+            System.out.println("Resultado " + numero + " * " + i + " = " + (numero*i));
+        }
     }
 }
