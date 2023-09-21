c79260bfddf326b6972d2dd4c127ac59d9ad5144
CaroBima <carobima@gmail.com>
2023-09-21 20:10:38 -0300

    Agregando operadores ternarios

--- src/main/java/com/curso/clase9/interfaces/funcionales/verificador/Principal.java
+++ src/main/java/com/curso/clase9/interfaces/funcionales/verificador/Principal.java
@@ -4,11 +4,12 @@ public class Principal {
 
     //ejemplo 2
     public static void realizarVerificacion(int num, Verificador verificador){
-        if(verificador.verificar(num)){
+        System.out.println(verificador.verificar(num) ? num + " cumple con la verificación" : num + " no cumple con la verificación");
+        /*if(verificador.verificar(num)){
             System.out.println(num + " cumple con la verificacion");
         }else{
             System.out.println(num + " no cumple con la verificacion");
-        }
+        }*/
     }
     public static void main(String[] args) {
 
@@ -18,15 +19,16 @@ public class Principal {
         Verificador esImpar = (a -> a%2 != 0); //ejemplo 1 y 2
 
 
-    /*
     //Ejemplo 1
         int a = 5;
-        if(esPar.verificar(a)){
+        System.out.println(esPar.verificar(a) ? a + " es par" : a + " es impar");
+
+        /*if(esPar.verificar(a)){
             System.out.println("Par");
         }else {
             System.out.println("Impar");
         }
-    */
+        */
 
         //Ejemplo 2
         realizarVerificacion(num, esImpar);
