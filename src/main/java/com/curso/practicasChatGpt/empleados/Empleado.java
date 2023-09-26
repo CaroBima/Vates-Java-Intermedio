eec68262cfe197cb59674a7b5391bcefd6f84429
CaroBima <carobima@gmail.com>
2023-09-26 10:37:13 -0300

    Comenzando resolución de ejercicio 3, puntos 1 y 2

--- src/main/java/com/curso/practicasChatGpt/empleados/Empleado.java
+++ src/main/java/com/curso/practicasChatGpt/empleados/Empleado.java
@@ -1,4 +1,40 @@
 package com.curso.practicasChatGpt.empleados;
 
 public class Empleado {
+    String nombre;
+    int edad;
+    double salario;
+
+    //constructores
+    public Empleado(String nombre, int edad, double salario) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salario = salario;
+    }
+
+    public Empleado(){}
+
+    //getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
--- src/main/java/com/curso/practicasChatGpt/empleados/Principal.java
+++ src/main/java/com/curso/practicasChatGpt/empleados/Principal.java
@@ -1,4 +1,75 @@
 package com.curso.practicasChatGpt.empleados;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/*
+Tu tarea es realizar las siguientes operaciones utilizando programación funcional:
+
+Filtrar los empleados que tienen un salario mayor o igual a $50,000.
+Calcular el promedio de edad de los empleados que tienen un salario mayor o igual a $50,000.
+Encontrar el empleado más joven entre aquellos que tienen un salario mayor o igual a $50,000.
+Crear una lista de nombres de empleados que tienen edades entre 25 y 35 años (inclusive) y un salario mayor o igual a $50,000.
+Imprimir en la consola el nombre y salario del empleado más joven con un salario mayor o igual a $50,000.
+Puedes crear una lista de empleados de ejemplo y realizar estas operaciones siguiendo el estilo de programación
+funcional que hemos estado utilizando.
+ */
 public class Principal {
+    public static void main(String[] args) {
+        List<Empleado> listaEmpleados = new ArrayList<>();
+
+        cargarEmpleados(listaEmpleados);
+
+        //Filtrar los empleados que tienen un salario mayor o igual a $50,000.
+        List<Empleado> mayoresSalarios = listaEmpleados.stream()
+                .filter(empleado -> {
+                    return empleado.getSalario()>=50000.0;
+                })
+                .collect(Collectors.toList());
+
+        //Calcular el promedio de edad de los empleados que tienen un salario mayor o igual a $50,000.
+        Double promedio = listaEmpleados.stream()
+                .mapToDouble(Empleado::getEdad)
+                .average()
+                .orElse(0.0);
+
+        //Encontrar el empleado más joven entre aquellos que tienen un salario mayor o igual a $50,000.
+        Optional<Empleado> empleadoMasJoven = listaEmpleados.stream()
+                .min(Comparator.comparingInt(empleMasJ -> empleMasJ.getEdad()));
+
+        empleadoMasJoven.ifPresent(emplMasJoven -> {
+            System.out.println("Empleado menor: " + emplMasJoven.getNombre());
+            System.out.println("Salario: " + emplMasJoven.getSalario());
+            System.out.println("Edad: " + emplMasJoven.getEdad());
+            }
+        );
+
+        //Crear una lista de nombres de empleados que tienen edades entre 25 y 35 años (inclusive) y un salario mayor o igual a $50,000.
+        
+    }
+
+    /**
+     * Método que carga la lista con datos de ejemplo para trabajar
+     * @param empleados
+     */
+    private static void cargarEmpleados(List<Empleado> empleados){
+        empleados.add(new Empleado("Empleado1", 30, 55000.0));
+        empleados.add(new Empleado("Empleado2", 28, 60000.0));
+        empleados.add(new Empleado("Empleado3", 35, 48000.0));
+        empleados.add(new Empleado("Empleado4", 25, 52000.0));
+        empleados.add(new Empleado("Empleado5", 32, 65000.0));
+        empleados.add(new Empleado("Empleado6", 40, 70000.0));
+        empleados.add(new Empleado("Empleado7", 27, 50000.0));
+        empleados.add(new Empleado("Empleado8", 33, 58000.0));
+        empleados.add(new Empleado("Empleado9", 29, 54000.0));
+        empleados.add(new Empleado("Empleado10", 26, 56000.0));
+        empleados.add(new Empleado("Empleado11", 31, 59000.0));
+        empleados.add(new Empleado("Empleado12", 24, 53000.0));
+        empleados.add(new Empleado("Empleado13", 36, 62000.0));
+        empleados.add(new Empleado("Empleado14", 39, 68000.0));
+        empleados.add(new Empleado("Empleado15", 28, 51000.0));
+    }
 }
